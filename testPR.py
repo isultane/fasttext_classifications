2cbd7da65f6baab357ec944608e9a1485bbbb99f
Sultan <to_sultan@yahoo.com>
2021-08-13 14:24:08 +0300

    tested single labels PRF

--- testPR.py
+++ testPR.py
@@ -1,15 +1,15 @@
 import fasttext
 
 #model = fasttext.train_supervised(input="ambari.valid")
-model = fasttext.load_model("model_cooking.bin")
+model = fasttext.load_model("./data/best_kfold_models/best_k5_Chromium_model.bin")
 
 '''
 print(model.predict("access control  security report"))
 print(model.predict("sql injection security report"))
 
 print(model.predict("xss security report"))
-
-print(model.test("./data/kfold_train_test_data/ft_k10_cve_cwe_summary_clean.train"))
 '''
+print(model.test("./data/bug_reports/Chromium.valid"))
 
-print(model.test_label("cooking.valid"))
+
+#print(model.test_label("./data/bug_reports/Chromium.valid"))
