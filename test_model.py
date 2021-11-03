e728439f21643f1820106938dc2a10e8fdd6313f
Techy <to_sultan@yahoo.com>
2021-11-03 17:23:11 +0300

    some testing codes

--- test_model.py
+++ test_model.py
@@ -1,14 +1,26 @@
 # Author: Sultan S. Alqahtani
 # Date: 06/16/2021 
 import fasttext
+from numpy import rint
 
-model = fasttext.load_model("model_cve.bin")
+def doPrediction(model, text):
+    val = model.predict(text, 1)
+    label = None
 
-#print(model.words)
-#print(model.labels)
+    if(val[0][0] == '__label__nonsec-report'):
+        label = 'not security'
+    else:
+        label = 'security'
+    
+    print("Text = "+text)
+    print("Label= "+ label)
+    print("Confidence Score = "+ str(val[1][0]))
+    
 
+if __name__ == '__main__':
+    model = fasttext.load_model("model_ambari.bin")
 
-print(model.predict("buffer overflow security report", k=3))
-print(model.predict("sql injection security report", k=3))
-print(model.predict("denail of service dos security report", k=3))
+    doPrediction(model, "buffer overflow not security report")
+    #print(model.words)
+    #print(model.labels)
 
