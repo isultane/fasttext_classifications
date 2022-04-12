bca77c617a94be2b73f2bbf915479002028cf469
Sultan <to_sultan@yahoo.com>
2022-04-13 02:51:19 +0300

    other algorithms - running time experiment

--- 4_other_classifications_models.py
+++ 4_other_classifications_models.py
@@ -8,52 +8,61 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.model_selection import KFold
 
+def train_LR():
+    print("time starts here...")
+
+def train_SVM():
+    print("time starts here ...")
+
+def train_RFC():
+    print("time starts here ...")
+
 def get_score(model, x_train, x_test, y_train, y_test):
     model.fit(x_train, y_train)
     return model.score(x_test, y_test)
+if __name__ == '__main__':
+    scores_l = []
+    scores_svm = []
+    scores_rf = []
 
-scores_l = []
-scores_svm = []
-scores_rf = []
+    # 'cooking.train' is just for testing the code. It is going to be updated with te real datasets after pre-processing. 
+    data = open('Chromium.train').readlines()
 
-# 'cooking.train' is just for testing the code. It is going to be updated with te real datasets after pre-processing. 
-data = open('cooking.train').readlines()
+    count_vect = CountVectorizer()
 
-count_vect = CountVectorizer()
+    print('Loading data ...')
+    labels, texts = ([], [])
+    for line in data:
+        label, text = line.split(' ', 1)
+        labels.append(label)
+        texts.append(text)
 
-print('Loading data ...')
-labels, texts = ([], [])
-for line in data:
-    label, text = line.split(' ', 1)
-    labels.append(label)
-    texts.append(text)
+    trainDF = pd.DataFrame()
+    trainDF['label'] = labels
+    trainDF['text'] = texts
 
-trainDF = pd.DataFrame()
-trainDF['label'] = labels
-trainDF['text'] = texts
+    # to fit the text in the dataframe
+    # You have to do some encoding before using fit. As it known fit() does not accept Strings.
+    count_vect = CountVectorizer()
+    matrix = count_vect.fit_transform(trainDF['text'])
+    encoder = LabelEncoder()
+    targets = encoder.fit_transform(trainDF['label'])
 
-# to fit the text in the dataframe
-# You have to do some encoding before using fit. As it known fit() does not accept Strings.
-count_vect = CountVectorizer()
-matrix = count_vect.fit_transform(trainDF['text'])
-encoder = LabelEncoder()
-targets = encoder.fit_transform(trainDF['label'])
-
-# kfold cross validation
-folds = KFold(n_splits=3)
-for train_index, test_index in folds.split(matrix):
-    # split data to train test
-    x_train, x_test, y_train, y_test = matrix[train_index], matrix[test_index],\
+    # kfold cross validation
+    folds = KFold(n_splits=10)
+    for train_index, test_index in folds.split(matrix):
+        # split data to train test
+        x_train, x_test, y_train, y_test = matrix[train_index], matrix[test_index],\
                                     targets[train_index], targets[test_index]
 
-    scores_l.append(get_score(LogisticRegression(), x_train, x_test, y_train, y_test))
-    scores_svm.append(get_score(SVC(), x_train, x_test, y_train, y_test))
-    scores_rf.append(get_score(RandomForestClassifier(n_estimators=40), x_train, x_test, y_train, y_test))
+        scores_l.append(get_score(LogisticRegression(), x_train, x_test, y_train, y_test))
+        scores_svm.append(get_score(SVC(), x_train, x_test, y_train, y_test))
+        scores_rf.append(get_score(RandomForestClassifier(n_estimators=40), x_train, x_test, y_train, y_test))
 
-print("Resuts after 3 fold cross validations ...")
-print("LogisticRegression(): ", scores_l)
-print("SVC(): ", scores_svm)
-print("RandomForestClassifier(): ", scores_svm)
+    print("Resuts after 10 fold cross validations ...")
+    print("LogisticRegression(): ", scores_l)
+    print("SVC(): ", scores_svm)
+    print("RandomForestClassifier(): ", scores_svm)
 
 # in this .py we need to run the same dataset on SVC, RFC, LR, and other to check the results and compare them with fasttext results.
 # Also, we need to comare the performance for each algoeithm along with the same processed data to fasttext performance.
