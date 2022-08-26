ef54a8b6f7e68d40626d38f4994f9a02526cb452
Sultan <to_sultan@yahoo.com>
2022-08-26 21:31:10 +0300

    testing some experiments

--- temp_test.py
+++ temp_test.py
@@ -1,58 +1,291 @@
+# Author: Sultan S. Alqahtani
+# Date: 06/16/2021 
+
+#import imp
+from genericpath import isfile
+from ntpath import join
+from pydoc import doc
 import string
-import fasttext
-import re
-import pandas as pd
-from yaml import tokens
-from nltk.tokenize import word_tokenize
+import numpy
+from tracemalloc import stop
+#from numpy import vectorize
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.model_selection import KFold
+
+from nltk import word_tokenize
+from collections import defaultdict
+from nltk import FreqDist
 from nltk.corpus import stopwords
+import os
+import random
+import string
+import pickle
+
+#ML algorithms
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier  
 
 
-# Returns a list of common english terms (words)
-def initialize_words():
-    content = None
-    with open('/home/sultan/Downloads/wordlist.txt') as f: # A file containing common english words
-        content = f.readlines()
-    return [word.rstrip('\n') for word in content]
 
-def parse_sentence(sentence, wordlist):
-    new_sentence = "" # output  
-    tokens = re.findall(r'\w+\b', sentence)
- #   tokens = word_tokenize(sentence)
-    # convert to lower cases
-    tokens = [w.lower() for w in tokens]
-    # prepare regex for char filtering
-    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
-    # remove punctuation from each word
-    tokens = [re_punc.sub(' ', w) for w in tokens]
-    # filter out stop words
-    stop_words = set(stopwords.words('english'))
-    tokens = [w for w in tokens if w not in stop_words]
-    for term in tokens:
-        new_sentence += parse_terms(term, wordlist)
-        new_sentence += " "
+stop_words = set(stopwords.words('english'))
+stop_words.add('said')
+stop_words.add('mr')
 
-    return " ".join(new_sentence.split())
+# BASE_DIR = "path to files"
+# LABELS = ['labels list']
+BSE_DIR = './data/balanced_data/'
 
-def parse_terms(term, wordlist):
-    words = []
-    word = find_word(term, wordlist)  
-    while word != None and len(word) > 2:
-        words.append(word)            
-        if len(term) == len(word): # Special case for when eating rest of word
-            break
-        term = term[len(word):]
-        word = find_word(term, wordlist)
-    return " ".join(words)
 
-def find_word(token, wordlist):
-    i = len(token) + 1
-    while i > 1:
-        i -= 1
-        if token[:i] in wordlist:
-            return token[:i]
-    return None 
+# def creat_data_set():
+#     with open('pathtofile', 'w', encoding='utf9') as outfile:
+#         for label in LABELS:
+#             dir = '%s%s' % (BASE_DIR, label)
+#             for filename in os.listdir(dir):
+#                 fullfilenme = '%s%s' % (dir, filename)
+#                 print(fullfilenme)
+#                 with open(fullfilenme, 'rb') as file:
+#                     text = file.read().decode(errors='replace').replace('\n', '')
+#                     outfile.write('%s\t%s\t%s\n' %  (label, filename, text))
 
-wordlist = initialize_words()
-sentence = "Support case insensitive mount pathshttp://www.nabble.com/Non-case-sensitive-nice-URL-tf2643746.htmlI use nice URL to mount some page in the usual way :mountBookmarkablePage('/Organization'  Organization.class);That is great  but it will make an error if a user enter the url by hand and decide to not use case sensitive. I mean  I can mount organization without the capital letter  but it would make the same error if the user use Capital after that.Is there a way of using Nice URL that is not case sensitive?ThanksMarc"
-print(parse_sentence(sentence, wordlist))
+def setup_docs(porject_data):
+    docs = [] #(label, text)
+    with open (porject_data, 'r', encoding='utf8') as datafile:
+        for row in datafile:
+            parts = row.split(' ', 1)
+            doc = (parts[0], parts[1].strip())
 
+            docs.append(doc)
+    return docs
+
+def clean_text(text):
+    # remove punctution 
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # convert to lower case
+    text = text.lower()
+    return text
+
+def get_tokens(text):
+    # get individual words
+    tokens = word_tokenize(text)
+    # remove common words that are useless
+    tokens = [t for t in tokens if not t in stop_words]
+    return tokens
+
+def print_frequency_dis(docs):
+    tokens = defaultdict(list)
+
+    # lets make a gaint list of all the words for ech category
+    for doc in docs:
+        doc_label = doc[0]
+        doc_text = clean_text(doc[1])
+
+        doc_tokens = get_tokens(doc_text)
+
+        tokens[doc_label].extend(doc_tokens)
+
+        for category_label, category_tokens in tokens.items():
+            print(category_label)
+            fd = FreqDist(category_tokens)
+            print(fd.most_common(20))
+
+def get_splits(docs):
+    #scramble docs
+    random.shuffle(docs)
+
+    X_train = []    #traingin documents
+    y_train = []    #corresponding training labels
+
+    X_test = []     #test documents
+    y_test = []     #corresponding testing labels
+
+    pivot = int(.80 * len(docs))
+
+    for i in range(0, pivot):
+        X_train.append(docs[i][1])
+        y_train.append(docs[i][0])
+
+    for i in range(pivot, len(docs)):
+        X_test.append(docs[i][1])
+        y_test.append(docs[i][0])
+
+    return X_train, X_test, y_train, y_test 
+
+
+def evaluate_clssifier(title, classifier, vectorizer, X_test, y_test):
+    X_test_tfidf = vectorizer.transform(X_test)
+    y_pred = classifier.predict(X_test_tfidf)
+
+    precision = metrics.precision_score(y_test, y_pred, average="binary", pos_label="__label__sec")
+    recall = metrics.recall_score(y_test, y_pred, average="binary", pos_label="__label__sec")
+    f1 = metrics.f1_score(y_test,y_pred, average="binary", pos_label="__label__sec")
+
+    print("%s\t%f\t%f\t%f\n" % (title, precision, recall, f1))
+
+def train_classifier(classifier_title,classifier_algorithm,docs):
+    #split document into 80% training and 20% testing
+    X_train, X_test, y_train, y_test = get_splits(docs)
+   
+    y_train = numpy.array(y_train)
+    X_train = numpy.array(X_train)
+
+    kf = KFold(n_splits=10)
+    kf.get_n_splits(X_train)
+    metrics = []
+    for train_index, test_index in kf.split(X_train, y_train):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X_train[train_index], X_train[test_index]
+        y_train, y_test = y_train[train_index], y_train[test_index]
+        vect = CountVectorizer(ngram_range=(1,2), max_features=1000 , 
+        stop_words="english")
+        X_train_dtm = vect.fit_transform(X_train)
+        X_test_dtm = vect.transform(X_test).toarray()
+        nb = classifier_algorithm
+        nb.fit(X_train_dtm, y_train)
+        y_pred_class = nb.predict(X_test_dtm)
+        
+        metrics.append(accuracy_score(y_test, y_pred_class))
+
+    metrics = numpy.array(metrics)
+    print('Mean accuracy: ', numpy.mean(metrics, axis=0))
+    print('Std for accuracy: ', numpy.std(metrics, axis=0))
+
+   
+    #  # the object that turns text into vectors 
+    # vectorizer = CountVectorizer(stop_words='english',ngram_range=(1,3),min_df=3, analyzer='word')
+
+    # # crete doc-term matrix
+    # if classifier_title is 'GaussianNB':
+    #     dtm = vectorizer.fit_transform(X_train).toarray()
+    # else:
+    #     dtm = vectorizer.fit_transform(X_train)
+
+    # # train the classfier 
+    # classifier = classifier_algorithm.fit(dtm, y_train)
+
+    # # evaluate_clssifier(classifier_title, classifier, vectorizer, X_train, y_train)
+    # # evaluate_clssifier(classifier_title, classifier, vectorizer, X_test, y_test)
+
+    # # X_test_tfidf = vectorizer.transform(X_test)
+    # # y_pred = classifier.predict(X_test_tfidf)   
+    
+    # # print(confusion_matrix(y_test,y_pred))
+    # # print(classification_report(y_test,y_pred))
+    # # print(accuracy_score(y_test, y_pred))
+
+    # # store the classifier
+    # clf_filename = classifier_title+'.pkl'
+    # pickle.dump(classifier, open(clf_filename, 'wb'))
+
+    # # also store the vectorizer so we can transform new data
+    # vec_filename = classifier_title+'_count_vectorizer.pkl'
+    # pickle.dump(vectorizer, open(vec_filename, 'wb'))
+
+def calculte_pf(y_test, predictions):
+    CM = confusion_matrix(y_test, predictions)
+    TN = CM[0][0]
+    #  FN = CM[1][0]
+    #  TP = CM[1][1]
+    FP = CM[0][1]
+    if FP == 0 and TN == 0:
+        pf = 1
+        return pf
+    else:
+        pf = FP / (FP + TN)
+        return pf
+    
+def validate(classifier_title,target_project, target_title, training_title):
+    X_target_train, X_target_test,y_target_train, y_target_test = get_splits(target_project)
+
+    #load classifier
+    clf_filename = classifier_title+'.pkl'
+    nb_clf = pickle.load(open(clf_filename, 'rb'))
+
+    #vectorize the new text
+    vec_filname = classifier_title+'_count_vectorizer.pkl'
+    vectorizer = pickle.load(open(vec_filname, 'rb'))
+
+    if classifier_title is 'GaussianNB':
+        y_pred = nb_clf.predict(vectorizer.transform(X_target_test).toarray())
+    else:
+        y_pred = nb_clf.predict(vectorizer.transform(X_target_test))
+
+   
+    report = classification_report(y_target_test, y_pred, output_dict=True )
+    precision =  report['macro avg']['precision'] 
+    recall = report['macro avg']['recall']    
+    f1_score = report['macro avg']['f1-score']
+    pf = calculte_pf(y_target_test, y_pred)
+    g_score = (2*recall*(1-pf))/(recall + (1-pf))
+
+    print("Writing the results of %s classifier after validating  %s project data.\n" % (classifier_title,target_title))
+    with open('updated_'+target_title, 'a') as file:
+        file.write('\n Results of ' + classifier_title + '_vs_' + training_title)
+        file.write('\n macro_precision : {}'.format(precision))
+        file.write('\n macro_recall : {}'.format(recall))
+        file.write('\n macro_f1 : {}'.format(f1_score))
+        file.write('\n pf : {}'.format(pf))
+        file.write('\n g_score : {}'.format(g_score))
+    
+    # print(confusion_matrix(y_target_test,y_pred))
+    # print(classification_report(y_target_test,y_pred))
+    # print(accuracy_score(y_target_test, y_pred))
+
+    print("Deleting tmp files: model and vectorizer files")
+    if os.path.exists(clf_filename):
+        os.remove(clf_filename)
+    if os.path.exists(vec_filname):
+        os.remove(vec_filname)
+
+
+if __name__ == '__main__':
+    #create_data_set()
+    projects_files = [f for f in os.listdir(BSE_DIR) if isfile(join(BSE_DIR, f))]
+
+    tested_projects = []
+    training_list = []
+    
+    for target_project in projects_files:
+        if target_project not in tested_projects:
+            tested_projects.append(target_project)
+        training_list = [i for i in projects_files if i not in tested_projects]
+        for train in training_list:
+            #preparing the training project data
+            print('Processing project:' + train)
+            docs = setup_docs(BSE_DIR+train)
+            #print_frequency_dis(docs)
+            #preparing the target project data 
+            new_docs = setup_docs(BSE_DIR+target_project)
+
+            train_classifier('LogisticRegression', LogisticRegression(),docs)
+            # validate('LogisticRegression',new_docs,target_project, train)
+
+            # train_classifier('RandomForestClassifier', RandomForestClassifier(),docs)
+            # validate('RandomForestClassifier',new_docs,target_project, train)
+
+            # train_classifier('GaussianNB', GaussianNB(),docs)
+            # validate('GaussianNB',new_docs,target_project, train)
+
+            # train_classifier('KNeighborsClassifier', KNeighborsClassifier(),docs)
+            # validate('KNeighborsClassifier',new_docs,target_project, train)
+
+            # train_classifier('MLPClassifier', MLPClassifier(),docs)
+            # validate('MLPClassifier',new_docs,target_project, train)
+
+            # validating target project
+            
+        training_list.clear()
+        tested_projects.clear()
+    print("Done!")
+
+    # useful link https://stackoverflow.com/questions/10592605/save-classifier-to-disk-in-scikit-learn 
+    # useful link https://stackabuse.com/text-classification-with-python-and-scikit-learn/
+    # finl version
